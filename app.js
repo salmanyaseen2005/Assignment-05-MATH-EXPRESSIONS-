646138008f4a6b3156c9f57f0c18970834e1643a
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 03:55:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,134 @@
+
+// Question 01
+var num1= 3
+var num2= 5
+
+var sum=  num1 + num2
+
+document.write("Sum of " +num1+" and "+num2+" is "+sum)
+
+// Question 02
+
+var num1= 3
+var num2= 5
+
+var sum=  num1 - num2
+
+document.write( "<br>" + "<br>" +"subtraction of " +num1+" and "+num2+" is "+sum)
+
+var num1= 3
+var num2= 5
+
+var sum=  num1 * num2
+
+document.write( "<br>" +"multiplication of " +num1+" and "+num2+" is "+sum)
+
+var num1= 3
+var num2= 5
+
+var sum=  num1 / num2
+
+document.write( "<br>" +"division of " +num1+" and "+num2+" is "+sum)
+
+var num1= 5
+var num2= 3
+
+var sum=  num1 % num2
+
+document.write( "<br>" +"modulus of " +num1+" and "+num2+" is "+sum)
+
+
+// Question 03
+
+
+// Question 04
+var movieticket = 600
+
+var buying = 5
+
+var cost = movieticket*buying
+
+document.write("<BR>"+"<BR>"+"Total Cost to buy "+buying+" ticket to a movie is "+cost+"PKR")
+
+// Question 05
+
+// Question 06
+
+// Question 07
+document.write(" <H1>Shopping Cart</H1>")
+var item1 =650
+var item2 =100
+
+var quantityitem1= 3
+var Quantityitem2 = 7
+
+var Shippingcharges =100
+
+
+var item1order= item1*quantityitem1
+var item2order = item2*Quantityitem2
+
+var totalprice =item1order+item2order+Shippingcharges
+
+document.write("<BR>"+"<BR>"+"Price of item 1 is "+item1)
+document.write("<BR>"+"<BR>"+"Quantity  of item 1 is "+quantityitem1)
+document.write("<BR>"+"<BR>"+"Price of item 2 is "+item2)
+document.write("<BR>"+"<BR>"+"Quantity of item 1 is "+Quantityitem2)
+document.write("<BR>"+"<BR>"+"Shippingcharg  "+Shippingcharges)
+document.write("<BR>"+"<BR>"+" Total    Cost of Your Order is  "+totalprice)
+
+// Question 08
+document.write( "<H1>Marks Sheet </H1>")
+var marks = 980
+var obtained =804
+
+per=(obtained/marks)*100
+
+
+document.write("Total marks:"+ marks+"<br>"+"Marks obtained:"+obtained+"<br>"+"Percentage"+per+"%")
+
+// Question 09
+// Question 10
+
+
+ var initialNumber = 7;
+
+var result = ((initialNumber + 5) * 10) / 2;
+
+console.log("Result:", result);
+
+// Question 11
+document.write("<h3>Age Calculator</h3>")
+
+var current=2016    
+var Birth=1992
+var age=current-Birth
+
+document.write("Curent Year: "+current+"<br>"+"Birth Year: "+Birth+"<br>"+"Your Age is: "+age)
+
+// Question 12
+// Question 13
+document.write("<h1>The Lifetime Supply Calculator</h2>")
+
+//var fs = ChocolateChip
+
+var Cage =   15
+
+var Mage =   65
+
+var  perday=  3
+
+var total= Mage-Cage
+
+var totaleat=total*perday
+
+
+document.write("Favourite Snack : Chocolate Chip"+"<br>"+"Current age: "+Cage+"<br>"+"Estimated Maximum Age:"+Mage+"<BR>")
+document.write("Amount of Snacks Per day:"+perday+"<BR>"+"<BR>"+" You Will need "+totaleat+" Chocolate Chip to last you until the ripe old age of "+Mage)
+
+
+
+
+
+
+
